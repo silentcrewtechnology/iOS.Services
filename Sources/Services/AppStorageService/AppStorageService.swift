77ee5e618288d1dd4c561657a65cf15bc87267eb
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-09-18 09:42:38 +0300

    PCABO3-11509: Поправил комментарии

--- Sources/Services/AppStorageService/AppStorageService.swift
+++ Sources/Services/AppStorageService/AppStorageService.swift
@@ -1,30 +1,14 @@
 import Foundation
 
 public class AppStorageService {
-    // TODO: - PCABO3-11586
-    public var deviceToken: String? {
-        get { UserDefaults.standard.string(forKey: "DeviceToken") }
-        set { UserDefaults.standard.setValue(newValue, forKey: "DeviceToken") }
-    }
+    // TODO: - PCABO3-11586 Keychain
     public var refreshToken: String? {
         get { UserDefaults.standard.string(forKey: "RefreshToken") }
         set { UserDefaults.standard.setValue(newValue, forKey: "RefreshToken") }
     }
-    public var sessionToken: String? {
-        get { UserDefaults.standard.string(forKey: "SessionToken") }
-        set { UserDefaults.standard.setValue(newValue, forKey: "SessionToken") }
-    }
-    public var bearerToken: String? {
-        get { UserDefaults.standard.string(forKey: "BearerToken") }
-        set { UserDefaults.standard.setValue(newValue, forKey: "BearerToken") }
-    }
     public var pin: String? {
         get { UserDefaults.standard.string(forKey: "Pin") }
         set { UserDefaults.standard.setValue(newValue, forKey: "Pin") }
-    }
-    public var pushToken: String? {
-        get { UserDefaults.standard.string(forKey: "PushToken") }
-        set { UserDefaults.standard.setValue(newValue, forKey: "PushToken") }
     }
     public var userFirstName: String? {
         get { UserDefaults.standard.string(forKey: "UserFirstName") }
@@ -33,6 +17,24 @@ public class AppStorageService {
     public var userPictureURL: String? {
         get { UserDefaults.standard.string(forKey: "UserPictureURL") }
         set { UserDefaults.standard.setValue(newValue, forKey: "UserPictureURL") }
+    }
+    
+    // TODO: - Store in memory / UserDefaults
+    public var pushToken: String? {
+        get { UserDefaults.standard.string(forKey: "PushToken") }
+        set { UserDefaults.standard.setValue(newValue, forKey: "PushToken") }
+    }
+    public var deviceToken: String? {
+        get { UserDefaults.standard.string(forKey: "DeviceToken") }
+        set { UserDefaults.standard.setValue(newValue, forKey: "DeviceToken") }
+    }
+    public var bearerToken: String? {
+        get { UserDefaults.standard.string(forKey: "BearerToken") }
+        set { UserDefaults.standard.setValue(newValue, forKey: "BearerToken") }
+    }
+    public var sessionToken: String? {
+        get { UserDefaults.standard.string(forKey: "SessionToken") }
+        set { UserDefaults.standard.setValue(newValue, forKey: "SessionToken") }
     }
     
     public init() { }
